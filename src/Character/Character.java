4098ae9ca6cc6c344f3299b3cbc83acb83dcac87
Louis <louis.senacsavall@ynov.com>
2021-10-28 11:51:31 +0200

    charachter exp endgame

--- src/Character/Character.java
+++ src/Character/Character.java
@@ -15,15 +15,21 @@ public class Character
     protected int m_LifePoint = 100;
     protected int m_Strength = 5;
     protected int m_Agility = 3;
-    protected int m_Defense;
+    protected int m_Defense = 10;
+    private int m_Experience = 0;
     private Vector<Item> m_Inventory = new Vector<Item>();
     private Weapons m_Weapons;
+    private int m_Level = 1;
 
 
 /***
  * Methods
  */
-public void attack() {
+public Character(String Name) {
+    m_Name = Name;
+}
+
+public void attack(Character enemy, Character Attacker) {
    /** try {
         Clip clip = AudioSystem.getClip();
         AudioInputStream inputStream = AudioSystem.getAudioInputStream(
@@ -33,20 +39,20 @@ public void attack() {
     } catch (Exception e) {
         System.err.println(e.getMessage());
     }**/
-
+    enemy.takeDamage(Attacker);
 }
-public void takeDamage(Character enemy) {
+public void takeDamage(Character Attacker) {
     int ProbabilityAgility = (int)(Math.random() * (100 + 1));
     if (ProbabilityAgility < m_Agility) {
         System.out.println("The enemy dodge the attack");
     } else {
         int ProbabilityStrength = (int)(Math.random() * (100 + 1));
-        if (ProbabilityStrength < enemy.m_Strength) {
-            m_LifePoint -= enemy.m_Weapons.getDamages() * 2;
-            System.out.println("Critical damage, the target take : " + enemy.m_Weapons.getDamages() * 2);
+        if (ProbabilityStrength < Attacker.m_Strength) {
+            m_LifePoint -= ((Attacker.m_Weapons.getDamages() * 2) - (m_Defense / 10));
+            System.out.println("Critical damage, the target take : " + Attacker.m_Weapons.getDamages() * 2);
         } else {
-            m_LifePoint -= enemy.m_Weapons.getDamages();
-            System.out.println("The target take : " + enemy.m_Weapons.getDamages());
+            m_LifePoint -= (Attacker.m_Weapons.getDamages() - (m_Defense /10));
+            System.out.println(m_Name +" take " + Attacker.m_Weapons.getDamages() + " his defense blocked " + m_Defense /10 +" damage");
         }
     }
 }
@@ -56,14 +62,45 @@ public void recoveryItem()
 
 }
 
-public void switchWeapons()
+public void switchWeapons(Weapons newWeapons)
 {
-
+    m_Weapons = newWeapons;
 }
 
 public void counterAttack()
 {
 
+}
+
+public void endCombat(Boolean win) {
+    m_LifePoint = 100;
+    // method for durability
+    //m_Weapons.setDurability();
+    // add exp
+    setExperience(win);
+    // elo + or -
+}
+
+public void Experience() {
+//    if (m_Experience <= 120) {
+//        m_Level = 1;
+//    } else if (m_Experience > 120 && m_Experience <=240) {
+//        m_Level = 2;
+//        m_Agility = 4;
+//        m_Strength = 6;
+//    } else if ( m_Experience > 240 && m_Experience <= 480) {
+//        m_Level = 3;
+//        m_Agility = 6;
+//        m_Strength = 8;
+//    } else if ( m_Experience > 480 && m_Experience <= 960) {
+//        m_Level = 4;
+//        m_Agility = 8;
+//        m_Strength = 10;
+//    } else if (m_Experience > 960 && m_Experience <= 1920){
+//        m_Level = 5;
+//        m_Agility = 10;
+//        m_Strength = 12;
+//    }
 }
 
 /***
@@ -72,7 +109,7 @@ public void counterAttack()
     public int getAgility() {
         return m_Agility;
     }
-    public int getStrenght() {
+    public int getStrength() {
         return m_Strength;
     }
     public int getLifePoint() {
@@ -82,6 +119,11 @@ public void counterAttack()
     {
         return m_Defense;
     }
+
+    public int getExperience() {
+        return m_Experience;
+    }
+
     public Vector<Item> getInventory() {
         return m_Inventory;
     }
@@ -93,27 +135,37 @@ public void counterAttack()
 */
 public void setLifePoint(int lifePoints)
 {
-    m_LifePoint = lifePoints;
+    this.m_LifePoint = lifePoints;
 }
     public void setAgility(int agility) {
-        m_Agility = agility;
+        this.m_Agility = agility;
     }
     public void setStrenght(int strenght) {
-        m_Strength = strenght;
+        this.m_Strength = strenght;
     }
     public void setDefense(int defense)
     {
-        m_Defense = defense;
+        this.m_Defense = defense;
     }
     public void setName(String Name){
-    m_Name = Name;
+    this.m_Name = Name;
         Logger.addLog("setName Character", m_Name, Logger.LOG_LEVEL.LOG);
     }
+
+    public void setExperience(boolean win) {
+        if (win) {
+            this.m_Experience += 40;
+        } else {
+            this.m_Experience += 13;
+        }
+
+    }
+
     public void setInventory(Item item) {
-        m_Inventory.add(item);
+        this.m_Inventory.add(item);
     }
     public void setWeapons(Weapons weapons) {
-        m_Weapons = weapons;
+        this.m_Weapons = weapons;
     }
 }
 
